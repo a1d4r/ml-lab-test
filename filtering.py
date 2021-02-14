f6f08795f456cf4b3430c88cf7044a93a214e923
Aidar Garikhanov <a1d4r@yandex.ru>
2021-02-14 11:06:50 +0300

    Load model and images

--- filtering.py
+++ filtering.py
@@ -1,1 +1,17 @@
 import torch
+import numpy as np
+import os
+from PIL import Image
+
+INPUT_DIR = './input'
+OUTPUT_DIR = './output'
+MODEL_PATH = 'model.pt'
+
+loaded_script = torch.jit.load(MODEL_PATH)
+
+images = []
+for file in os.listdir(INPUT_DIR):
+    if file.endswith('.png'):
+        images.append(Image.open(os.path.join(INPUT_DIR, file)))
+
+print(images)
