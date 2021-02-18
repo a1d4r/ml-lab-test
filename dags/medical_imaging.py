6bf6b3b75a01533a6b87b3516c800376302c748d
Aidar Garikhanov <a1d4r@yandex.ru>
2021-02-18 14:17:53 +0300

    Complete DAG

--- dags/medical_imaging.py
+++ dags/medical_imaging.py
@@ -1,18 +1,19 @@
-import json
 import torch
 import os
 from PIL import Image
+from PIL.Image import Image as ImageType
 from torchvision import transforms
 import shutil
-from typing import List, Tuple
+from typing import List
 from datetime import timedelta
 
 from airflow.decorators import dag, task
 from airflow.utils.dates import days_ago
 
-INPUT_DIR = './input'
-OUTPUT_DIR = './output'
-MODEL_PATH = 'model.pt'
+INPUT_DIR = '/home/aidar/Projects/PycharmProjects/ml_lab_test/data/input'
+OUTPUT_DIR = '/home/aidar/Projects/PycharmProjects/ml_lab_test/data/output'
+TENSOR_PATH = '/home/aidar/Projects/PycharmProjects/ml_lab_test/data/tensor/tensor.pt'
+MODEL_PATH = '/home/aidar/Projects/PycharmProjects/ml_lab_test/models/model.pt'
 THRESHOLD = 0.1
 
 default_args = {
@@ -21,7 +22,7 @@ default_args = {
     'email': ['a1d4r@example.com'],
     'email_on_failure': False,
     'email_on_retry': False,
-    'retries': 1,
+    'retries': 0,
     'retry_delay': timedelta(minutes=5),
 }
 
@@ -39,7 +40,7 @@ transform_L = transforms.Compose([
 ])
 
 
-def transform(image):
+def transform(image: ImageType):
     if image.mode == 'RGB':
         return transform_RGB(image)
     elif image.mode == 'L':
@@ -51,40 +52,39 @@ def transform(image):
 @dag(default_args=default_args, schedule_interval=None, start_date=days_ago(2))
 def filter_images():
     """Filter medical images"""
-    @task(multiple_outputs=True)
-    def load() -> List[Tuple[str, Image]]:
-        """Load images"""
+    @task()
+    def load_and_preprocess() -> List[str]:
+        """Preprocess images."""
         images = []
-        for file in os.listdir(INPUT_DIR):
-            if file.endswith('.png'):
-                images.append((file, Image.open(os.path.join(INPUT_DIR, file))))
-        return images
+        image_names = []
+        for filename in os.listdir(INPUT_DIR):
+            if filename.endswith('.png'):
+                image_names.append(filename)
+                images.append(Image.open(os.path.join(INPUT_DIR, filename)))
+        tensor = torch.cat([transform(image).unsqueeze(0) for image in images], dim=0)
+        torch.save(tensor, TENSOR_PATH)
+        return image_names
 
-    @task(multiple_outputs=True)
-    def transform(images: List[Tuple[str, Image]]) -> torch.Tensor:
-        """Transform images."""
-        return torch.cat([transform(image).unsqueeze(0) for _, image in images], dim=0)
-
-    @task(multiple_outputs=True)
-    def filter(tensor, images: List[Image]) -> List[Tuple[str, Image]]:
+    @task()
+    def filter(image_names: List[str]) -> List[str]:
         """Filter images."""
         # Run loaded script
+        tensor = torch.load(TENSOR_PATH)
         loaded_script = torch.jit.load(MODEL_PATH)
         result = loaded_script(tensor)
 
-        return [(file, image)
-                for tensor, (file, image) in zip(result, images)
+        return [filename
+                for tensor, filename in zip(result, image_names)
                 if tensor[0].item() < THRESHOLD]
 
     @task()
-    def save(filtered_images: List[Tuple[str, Image]]) -> None:
+    def save(filtered_images: List[str]) -> None:
         """Save images."""
-        for file, _ in filtered_images:
+        for file in filtered_images:
             shutil.copy2(os.path.join(INPUT_DIR, file), OUTPUT_DIR)
 
-    images = load()
-    tensor = transform(images)
-    filtered_images = filter(tensor, images)
+    image_names = load_and_preprocess()
+    filtered_images = filter(image_names)
     save(filtered_images)
 
 
